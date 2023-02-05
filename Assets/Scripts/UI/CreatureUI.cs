19a713b9f0747818ab37b8196e576f5f69b5dae9
Sergio Lecuona <serlecu@hotmail.com>
2023-02-05 15:08:46 +0100

    Update CreatureUI component
    - Add public method SetCreatureID that updates the sprites given an ID
    - Move update sprites of ImageComponents to a function
    - Add CreatureSessionLog and CreatureSpritePool references

--- Assets/Scripts/UI/CreatureUI.cs
+++ Assets/Scripts/UI/CreatureUI.cs
@@ -5,6 +5,12 @@ using UnityEngine.UI;
 
 public class CreatureUI : MonoBehaviour
 {
+  [Header("Target Creature")]
+  private CreatureSessionLog creatureSessionLog;
+  private CreatureSpritePool creatureSpritePool;
+  private Creature creature;
+  [SerializeField] private string creatureID;
+
   [Header("Attributes")]
   [SerializeField] private Sprite bodySprite;
   [Range(0, 360)]
@@ -21,16 +27,62 @@ public class CreatureUI : MonoBehaviour
 
   private void Awake()
   {
+    creatureSessionLog = FindObjectOfType<CreatureSessionLog>();
+    if (creatureSessionLog == null) {
+      Debug.LogError("CreatureSessionLog not found.");
+    }
+
+    creatureSpritePool = FindObjectOfType<CreatureSpritePool>();
+    if (creatureSpritePool == null) {
+      Debug.LogError("CreatureSpritePool not found.");
+    }
+  }
+
+  private void Start()
+  {
+    if (creatureID != null)
+    {
+      UpdateCreatureData();
+    }
   }
 
   private void OnValidate()
   {
+    SetCreatureAspect();
+  }
+
+  private void UpdateCreatureData()
+  {
+    creature = creatureSessionLog.GetCreatureByID(creatureID);
+
+    if (creature != null)
+    {
+      creatureID = creature.id;
+      hueColor = creatureSpritePool.hueColor[creature.color];
+      bodySprite = creatureSpritePool.bodySprites[creature.form];
+      eyesSprite = creatureSpritePool.eyesSprites[creature.eye];
+      mouthSprite = creatureSpritePool.mouthSprites[creature.mouth];
+    } 
+    else
+    {
+      Debug.LogError("CreatureSessionLog not found.");
+    }
+
+    SetCreatureAspect();
+  }
+  private void SetCreatureAspect()
+  {
     bodyImage.sprite = bodySprite;
     hue = Mathf.InverseLerp(0, 360, (float)hueColor);
     bodyImage.color = Color.HSVToRGB(hue, 1, 1);
-
     eyesImage.sprite = eyesSprite;
     mouthImage.sprite = mouthSprite;
+  }
+
+  public void SetCreatureID(string id)
+  {
+    creatureID = id;
+    UpdateCreatureData();
   }
 
 
