0fd3e1c7a010645c0becd92d58acb4e40d520a9f
Sergio Lecuona <serlecu@hotmail.com>
2023-02-08 10:22:36 +0100

    Update MusicManager.cs
    - Update Descriptive comment
    - Add Events (onLastClip, onLoopEnd, onPlayEnd)
    - AudioSource[] made private
    - Add Loop method
    - Update Update() to allow "play once" functionality and UnityEvent calls.

--- Assets/Scripts/Sound/MusicManager.cs
+++ Assets/Scripts/Sound/MusicManager.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Events;
 
 /**
   This should be a singleton, so it is common for all scenes.
   It will play the audio loops in queue providing a continuous sound stream
   trough the game but allowing to switch between loops for different scenes or 
   game events.
+
   When we want to change the music, we call the method ChangeMusic() and pass
-  a list of the new Theme clips and optionally a fill clip to make a smoother
+  a list of the new Theme clips. Optionally pass a fillClip to make a smoother
   transition between themes.
+
+  Use method Loop() to set the loop mode on or off. Deafults ON.
+
+  Event onLastClip is called when the last clip of the list is loaded for 
+  event sequencing purposes.
+
+  Event onLoopEnd is called when the last clip of the loop list is endend.
+
+  Event onPlayEnd is called when the last clip of the list is endend.
 */
 
 public class MusicManager : MonoBehaviour
@@ -19,14 +30,20 @@ public class MusicManager : MonoBehaviour
     public static GameObject musicManagerObject = null;
     public AudioMixerGroup musicMixerGroup;
     public List<AudioClip> defaultMusicClips;
+    public List<AudioClip> audioClipList;
+    public UnityEvent onLastClip;
+    public UnityEvent onLoopEnd;
+    public UnityEvent onPlayEnd;
+
     private AudioSource musicSource;
     private AudioSource musicFillSource;
-
-    public List<AudioClip> audioClipList;
-    public AudioSource[] audioSourceArray;
+    private AudioSource[] audioSourceArray; 
+    private string lastLoopClip;   
     private double nextStartTime;
     private int toggle = 0;
     private int clipsInLoop = 1;
+    private bool doLoop = true;
+    private bool isPlaying = false;
 
     void Awake()
     {
@@ -57,6 +74,9 @@ public class MusicManager : MonoBehaviour
         } 
         else if (defaultMusicClips.Count != 0 && audioClipList.Count == 0)
         {
+            clipsInLoop = defaultMusicClips.Count;
+            lastLoopClip = defaultMusicClips.Last().name;
+
             foreach (AudioClip clip in defaultMusicClips)
             {
                 audioClipList.Add(clip);
@@ -76,20 +96,41 @@ public class MusicManager : MonoBehaviour
     {
         if(AudioSettings.dspTime > nextStartTime - 0.5)
         {
-            AudioClip clipToPlay = audioClipList[0];
+            if (audioClipList.Count > 0 ){
+                if (!isPlaying){
+                      isPlaying = true;
+                      nextStartTime = AudioSettings.dspTime + 0.2;
+                }
 
-            audioSourceArray[toggle].clip = clipToPlay;
-            audioSourceArray[toggle].PlayScheduled(nextStartTime);
+                AudioClip clipToPlay = audioClipList[0];
 
-            double duration = (double)clipToPlay.samples / clipToPlay.frequency;
-            nextStartTime = nextStartTime + duration;
+                audioSourceArray[toggle].clip = clipToPlay;
+                audioSourceArray[toggle].PlayScheduled(nextStartTime);
 
-            toggle = 1 - toggle;
-            if (audioClipList.Count <= clipsInLoop) {
-                audioClipList.Add(audioClipList[0]);
-  
+                double duration = (double)clipToPlay.samples / clipToPlay.frequency;
+                nextStartTime = nextStartTime + duration;
+
+                toggle = 1 - toggle;
+
+                if (audioClipList.Count <= clipsInLoop) {
+                    audioClipList.Add(audioClipList[0]);
+                }
+
+                audioClipList.RemoveAt(0);
+
+                if (clicToPlay.name == lastLoopClip && onLoopEnd != null){
+                    onLoopEnd.Invoke();
+                } 
             }
-            audioClipList.RemoveAt(0);
+
+            if (audioClipList.Count == 0 && isPlaying){
+                if (onLastClip != null) onLastClip.Invoke();
+            }
+
+        } else if (AudioSettings.dspTime >= nextStartTime)
+        {
+            isPlaying = false;
+            if (onplayEnd != null) onPlayEnd.Invoke();
         }
     }
 
@@ -98,6 +139,7 @@ public class MusicManager : MonoBehaviour
         audioClipList.Clear();
 
         clipsInLoop = musica.Count;
+        lastLoopClip = musica.Last().name;
 
         if (musicaFill != null)
         {
@@ -108,5 +150,10 @@ public class MusicManager : MonoBehaviour
         {
             audioClipList.Add(clip);
         }
+    }
+
+    public void Loop(bool doLoop)
+    {
+        this.doLoop = doLoop;
     }
 }
